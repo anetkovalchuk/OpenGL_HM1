afd43461fedaaa63632fbb114fda514a90244796
Anna <anetkovalchuk@knu.ua>
2026-04-17 04:46:55 +0300

    OpenGL project

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL())
+        return -1;
+
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // вихід по ESC
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true);
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
